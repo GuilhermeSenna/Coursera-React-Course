06f1f057b109b0185f7fb023bad3250ea5e61706
zBiggui <bigguisenna@gmail.com>
2021-11-13 20:51:42 -0300

    Início do curso

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div ClassName="container">
+          <NavbarBrand href="/">
+            teste
+          </NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
