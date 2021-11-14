1d3b57e50462c8678cb80b8e9cd0bb5c617e3a1c
zBiggui <bigguisenna@gmail.com>
2021-11-14 01:19:22 -0300

    Final da semana 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div ClassName="container">
-          <NavbarBrand href="/">
-            teste
-          </NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div ClassName="container">
+            <NavbarBrand href="/">
+              teste
+            </NavbarBrand>
+          </div>
+        </Navbar>
+
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
